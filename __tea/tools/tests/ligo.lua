d492ba5ceb347f200be56a79b174b11edfec5c80
Steven J <sjehann@gmail.com>
2022-09-06 22:52:35 +0200

    add project-root flag

--- __tea/tools/tests/ligo.lua
+++ __tea/tools/tests/ligo.lua
@@ -1,5 +1,5 @@
 local _computed = require "__tea.common.computed"
 
-local _cmd = _computed.LIGO_VARS.LIGO .. " run test ${ROOT}"
+local _cmd = _computed.LIGO_VARS.LIGO .. " run test --project-root . ${ROOT}"
 os.execute(string.interpolate(_cmd, _computed.TEST_VARS))
 -- ligo reports failure on its own
