7013f07ed45d41f3177f3cf90ae274dea01a3be5
Steven J <sjehann@gmail.com>
2022-11-19 14:44:24 +0100

    prettier

--- web/tests/base.mjs
+++ web/tests/base.mjs
@@ -12,26 +12,26 @@ const BOB_KEY = "edsk3RFfvaFaxbHx8BMtEW1rKQcPtDML3LXjNqMNLCzC3wLC1bWbAt";
 const RPC_URL = `http://localhost:${get(appHjson, "sandbox.rpc_port", 20000)}`;
 
 export const setup = async () => {
-  const { id } = appHjson;
-  const { contractAddress } = JSON.parse(
-    (await fs.readFile(`../deploy/sandbox-${id}.json`)).toString()
-  );
-  const admin = new AdminContract(RPC_URL, contractAddress, {
-    test: true,
-    signer: await InMemorySigner.fromSecretKey(ADMIN_KEY),
-  });
-  const bob = new Contract(RPC_URL, contractAddress, {
-    test: true,
-    signer: await InMemorySigner.fromSecretKey(BOB_KEY),
-  });
-  const bobAsAdmin = new AdminContract(RPC_URL, contractAddress, {
-    test: true,
-    signer: await InMemorySigner.fromSecretKey(BOB_KEY),
-  });
-  return {
-    bob,
-    admin,
-    bobAsAdmin,
-    contractAddress,
-  };
+    const { id } = appHjson;
+    const { contractAddress } = JSON.parse(
+        (await fs.readFile(`../deploy/sandbox-${id}.json`)).toString()
+    );
+    const admin = new AdminContract(RPC_URL, contractAddress, {
+        test: true,
+        signer: await InMemorySigner.fromSecretKey(ADMIN_KEY),
+    });
+    const bob = new Contract(RPC_URL, contractAddress, {
+        test: true,
+        signer: await InMemorySigner.fromSecretKey(BOB_KEY),
+    });
+    const bobAsAdmin = new AdminContract(RPC_URL, contractAddress, {
+        test: true,
+        signer: await InMemorySigner.fromSecretKey(BOB_KEY),
+    });
+    return {
+        bob,
+        admin,
+        bobAsAdmin,
+        contractAddress,
+    };
 };
